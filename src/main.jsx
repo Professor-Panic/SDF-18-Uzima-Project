6549d687d48a5fb9557fd0518e924e05ae9f6f3c
Derick <dericknyamao50@gmail.com>
2026-08-12 00:22:06 +0300

    sidebar component

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { ShowMap } from "./Map/App";
-import "./Map/index.css";
+import "./index.css";
+import Sidebar from "./Components/Layout/Sidebar";
 
 createRoot(document.getElementById("root")).render(
     <StrictMode>
+        <div className="flex h-screen bg-[#f7f9fc]">
+      <Sidebar />
+      <div className="flex-1">
         <ShowMap />
+      </div>
+    </div>
+        
+        
     </StrictMode>
 );

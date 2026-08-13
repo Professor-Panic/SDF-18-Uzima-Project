059ac9c987542c72f37441fd46ef2b8c4f8ff270
Derick <dericknyamao50@gmail.com>
2026-08-14 00:58:30 +0300

    edited side bar nav

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,6 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import AppointmentPage from "./Appointments/AppointmentsPage";
 import Header from "./Dashboard/Dashboard"
 import { ShowMap } from "./Map/Map";
-import {Sidebar} from "./Components/Sidebar"
 createRoot(document.getElementById("root")).render(
    <StrictMode>
       <BrowserRouter>
@@ -13,7 +12,7 @@ createRoot(document.getElementById("root")).render(
          <Route path="/dashboard" element={<Header/>}/>
          <Route path="/appointments" element={<AppointmentPage/>}/>
          <Route path="/map" element={<ShowMap/>}/>
-         <Route path="/sidebar" element={<Sidebar/>}/>
+         
       </Routes>
       </BrowserRouter>
    </StrictMode>,
